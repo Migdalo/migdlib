bb50d4c1fd8c98e018b200a6d0d81acc854c3cad
Migdalo <Migdalo@users.noreply.github.com>
2016-12-06 00:01:35 +0200

    Added option to exclude digits from rotation.

--- ciphers/caesar.py
+++ ciphers/caesar.py
@@ -1,10 +1,10 @@
-import string, argparse
+import string
+import argparse
 
-'''
-Caesar cipher
+""" Caesar cipher """
 
-'''
-def discover_alphabet(convertable):
+""" Get an alphabet range to use. """
+def discover_alphabet(convertable, nonumbers):
     alphabet = []
     has_upper = False
     has_lower = False
@@ -15,9 +15,11 @@ def discover_alphabet(convertable):
                 has_lower = True
             elif c in string.ascii_uppercase:
                 has_upper = True
-            elif c in string.digits:
+            elif not nonumbers and c in string.digits:
                 has_digits = True
         if has_digits and has_lower and has_upper:
+            break
+        elif nonumbers and has_lower and has_upper:
             break
             
     if has_lower:
@@ -28,21 +30,23 @@ def discover_alphabet(convertable):
         alphabet.append(string.digits)
     return ''.join(alphabet)
 
-def rotate(convertable, rot=13, alphabet=''):
+""" Rotate characters """
+def rotate(convertable, rot=13, alphabet='', nonumbers=False):
     plain = []
     
     if not alphabet:
-        alphabet = discover_alphabet(convertable)
+        alphabet = discover_alphabet(convertable, nonumbers)
     
     # If rotation is larger than the alphabet length
     if rot > len(alphabet):
-        print type(rot), type(len(alphabet)), rot
         rot = rot - len(alphabet)
     
     # Rotate all characters in the input string
     try:
         for c in convertable:
-            if c not in alphabet:
+            if nonumbers and c in string.digits:
+                plain.append(c)
+            elif c not in alphabet:
                 plain.append(c)
             else:
                 tmp = alphabet.index(c) + rot
@@ -53,37 +57,39 @@ def rotate(convertable, rot=13, alphabet=''):
     except:
         return ''
 
-def solve_rotation(convertable, keyword):
-    alphabet = discover_alphabet(convertable)
-    print '{0: <5}'.format('ROT'), convertable
-    print '-' * 20
+""" Brute-force all possible rotations """
+def solve_rotation(convertable, keyword, nonumbers):
+    alphabet = discover_alphabet(convertable, nonumbers)
+    print('{0: <5}'.format('ROT'), convertable)
+    print('-' * 20)
     
     # Print all possible rotations
     for i in range(len(alphabet)):
         if not keyword:
-            print '{0: <5}'.format(str(i)), rotate(convertable, i, alphabet)
+            print('{0: <5}'.format(str(i)), rotate(convertable, i, alphabet, nonumbers))
         else:
-            solution = rotate(convertable, i, alphabet)
+            solution = rotate(convertable, i, alphabet, nonumbers)
             if keyword in solution:
-                print '{0: <5}'.format(str(i)), solution
+                print('{0: <5}'.format(str(i)), solution)
                 return
 
 def process_args():
     parser = argparse.ArgumentParser(description='Encode and decode Caesar cipher.', epilog='Developer: Migdalo')
-    parser.add_argument('cipher', help='The text you want to rotate.')
+    parser.add_argument('text', help='The text you want to rotate.')
     group = parser.add_mutually_exclusive_group()
     group.add_argument('-r', '--rotation', type=int, help='Number of rotations you want to do. ' +
             'Use negative numbers to decipher. Default is 13.')
     group.add_argument('-b', '--brute', action='store_true', help='Print all rotation options.')
     parser.add_argument('-k',  '--keyword', help='Keyword you want to find. Usable with -b.')
+    parser.add_argument('-n', '--nonumbers', action='store_true', help='Don\'t rotate numbers.')
     args = parser.parse_args()
     
     if args.brute:
-        solve_rotation(args.cipher, args.keyword)
+        solve_rotation(args.text, args.keyword, args.nonumbers)
     elif args.rotation:
-        print rotate(args.cipher, args.rotation)
+        print(rotate(args.text, args.rotation, nonumbers=args.nonumbers))
     else:
-        print rotate(args.cipher)
+        print(rotate(args.text, nonumbers=args.nonumbers))
 
 if __name__ == '__main__':
     process_args()
