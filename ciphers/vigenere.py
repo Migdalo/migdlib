a313dd865f8a199ec98e93d5fcf06f1f02bffafd
Migdalo <Migdalo@users.noreply.github.com>
2016-12-12 00:42:23 +0200

    Add an option to use a custom alphabet

--- ciphers/vigenere.py
+++ ciphers/vigenere.py
@@ -1,54 +1,85 @@
 #!/usr/bin/env python
+
 """ Vigenere cipher """
 import argparse
 import string
+import re
+import sys
 
-""" Helper functions """
-def get_alphabet_line(row):
+""" Get a character from an alphabetline. """
+def get_index(alphabet, keychar):
+    try:
+        return alphabet.index(keychar)
+    except ValueError as e:
+        print('ValueError: Could not find ' + keychar + ' from:')
+        print(alphabet)
+        print('Check the alphabet.')
+        sys.exit(1)
+
+""" Get the relevant line from Vigenere square """
+def get_alphabet_line(row, alphabet=string.ascii_uppercase):
     line = []
     # Create alphabet line
-    for i in range(len(string.ascii_uppercase)):
+    for i in range(len(alphabet)):
         shift = i + row
-        if shift >= len(string.ascii_uppercase):
-            shift -= len(string.ascii_uppercase)
-        line.append(string.ascii_uppercase[shift])
+        if shift >= len(alphabet):
+            shift -= len(alphabet)
+        line.append(alphabet[shift])
     return line
 
-def get_char_from_square(plainchar, keychar):
-    column = string.ascii_uppercase.index(plainchar)
-    row = string.ascii_uppercase.index(keychar)
-    alphabet = get_alphabet_line(row)
-    return alphabet[column]
+""" Get encrypted char from Vigenere square """
+def get_char_from_square(plainchar, keychar, alphabet):
+    column = get_index(alphabet, plainchar)
+    row = get_index(alphabet, keychar)
+    alphabetline = get_alphabet_line(row, alphabet)
+    return alphabetline[column]
 
-def get_label_from_square(plainchar, keychar):
-    row = string.ascii_uppercase.index(keychar)
-    alphabet = get_alphabet_line(row)
-    return string.ascii_uppercase[alphabet.index(plainchar)]
-    
-""" Vigenere """
-def morph_vigenere(text, key, morph_function):
-    text = text.upper()
+""" Get a plaintext char from Vigenere square  """
+def get_label_from_square(plainchar, keychar, alphabet):
+    row = get_index(alphabet, keychar)
+    alphabetline = get_alphabet_line(row, alphabet)
+    return alphabet[alphabetline.index(plainchar)]
+
+""" General Vigenere """
+def morph_vigenere(text, key, morph_function, alphabet=''):
+    a = string.ascii_uppercase
     morphtext = []
+    if alphabet:
+        alphabet = regex_to_string(alphabet)
+    else:
+        text = text.upper()
+        key = key.upper()
+        alphabet = string.ascii_uppercase
     i = 0
     for c in text:
         try:
             key[i]
         except:
             i = 0
-        if c not in string.ascii_uppercase:
+        if c not in alphabet:
             morphtext.append(c)
         else:
-            morphtext.append(morph_function(c, key[i]))
+            morphtext.append(morph_function(c, key[i], alphabet))
             i += 1
     return ''.join(morphtext)
-    
+
 """ Encryption """
-def encrypt_vigenere(plaintext, key):
-    return morph_vigenere(plaintext, key, get_char_from_square)
+def encrypt_vigenere(plaintext, key, alphabet=''):
+    return morph_vigenere(plaintext, key, get_char_from_square, alphabet)
 
 """ Decryption """
-def decrypt_vigenere(ciphertext, key):
-    return morph_vigenere(ciphertext, key, get_label_from_square)
+def decrypt_vigenere(ciphertext, key, alphabet=''):
+    return morph_vigenere(ciphertext, key, get_label_from_square, alphabet)
+
+""" Parse input regex alphabet to a string """
+def regex_to_string(regex):
+    alphabet = ''
+    allchars = string.ascii_lowercase + string.ascii_uppercase \
+               + string.digits + string.punctuation
+    for c in allchars:
+        if re.search(regex, c):
+            alphabet += c
+    return alphabet
 
 """ Main """
 if __name__ == '__main__':
@@ -58,9 +89,10 @@ if __name__ == '__main__':
     group.add_argument('-d', '--decrypt', action='store_true', help='Decrypt Vigenere cipher.')
     parser.add_argument('key', help='Keyword you want to use to encrypt/decrypt a string.')
     parser.add_argument('input', help='String that you want to encrypt/decrypt.')
+    parser.add_argument('-a', '--alphabet', help='Alphabet to use. E.g. [a-zA-Z{}]')
     args = parser.parse_args()
     
     if args.encrypt:
-        print(encrypt_vigenere(args.input, args.key))
+        print(encrypt_vigenere(args.input, args.key, args.alphabet))
     elif args.decrypt:
-        print(decrypt_vigenere(args.input, args.key))
+        print(decrypt_vigenere(args.input, args.key, args.alphabet))
