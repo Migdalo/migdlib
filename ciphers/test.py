392b2213f98f5c05630f90c487da483f58b552bb
Migdalo <Migdalo@users.noreply.github.com>
2016-12-12 00:44:15 +0200

    Add more tests

--- ciphers/test.py
+++ ciphers/test.py
@@ -5,6 +5,7 @@ import railfence
 import os, sys
 import playfair
 import transposition
+import vigenere
 
 if os.name == 'posix' and sys.version_info[0] < 3:
     try:
@@ -19,6 +20,9 @@ class TestCaesarCipher(unittest.TestCase):
     def test_rotation(self):
         self.assertEqual(caesar_cipher.rotate('r0t4t10n_t3st', 13), '4d6h6ed0_6g56')
         
+    def test_reverse_rotation(self):
+        self.assertEqual(caesar_cipher.rotate('4d6h6ed0_6g56', -13), 'r0t4t10n_t3st')
+        
     @unittest.expectedFailure
     def test_rotation_fail(self):
         self.assertEqual(caesar_cipher.rotate('r0t4t10n_t3st', 13, nonumbers=True), '4d6h6ed0_6g56')
@@ -31,10 +35,12 @@ class TestRailFenceCipher(unittest.TestCase):
         self.assertEqual(p, 'Can\'t use both -e and -d at the same time.\n')
     
     def test_encryption_simple(self):
-        self.assertEqual(railfence.encrypt_rail_fence('WE ARE DISCOVERED. FLEE AT ONCE', 3), 'WECRFACERDSOEE.LETNEAIVDEO')
+        self.assertEqual(railfence.encrypt_rail_fence( \
+        'WE ARE DISCOVERED. FLEE AT ONCE', 3), 'WECRFACERDSOEE.LETNEAIVDEO')
     
     def test_decryption_simple(self):
-        self.assertEqual(railfence.decrypt_rail_fence('WECRFACERDSOEE.LETNEAIVDEO', 3), 'WEAREDISCOVERED.FLEEATONCE')
+        self.assertEqual(railfence.decrypt_rail_fence( \
+        'WECRFACERDSOEE.LETNEAIVDEO', 3), 'WEAREDISCOVERED.FLEEATONCE')
     
     def test_encryption_long(self):
         self.assertEqual(railfence.encrypt_rail_fence('A-fence-is-a-structure-that-encloses-an-area,-SharifCTF{QmFzZTY0IGlzIGEgZ2VuZXJpYyB0ZXJt},-typically-outdoors.', 21), 'AaY--rpyfneJBeaaX0n-,ZZcs-uXeeSVJ-sh2tioaZ}slrg,-ciE-anfGt.-eCIyss-TzprttFliora{GcouhQIadctm0ltt-FYluuezTyorZ-')
@@ -45,10 +51,12 @@ class TestRailFenceCipher(unittest.TestCase):
 """ Test Playfair cipher """
 class TestPlayfair(unittest.TestCase):
     def test_encryption(self):
-        self.assertEqual(playfair.encode_playfair('Hide the gold in the tree stump', 'playfairexample'), 'BMNDWCXDKYBEKUDMLCXEIUUTIF')
+        self.assertEqual(playfair.encode_playfair('Hide the gold in the tree stump', \
+         'playfairexample'), 'BMNDWCXDKYBEKUDMLCXEIUUTIF')
     
     def test_decryption(self):
-        self.assertEqual(playfair.decode_playfair('BMNDWCXDKYBEKUDMLCXEIUUTIF', 'playfairexample'), 'HIDETHEGOLDINTHETREXESTUMP')
+        self.assertEqual(playfair.decode_playfair('BMNDWCXDKYBEKUDMLCXEIUUTIF', \
+        'playfairexample'), 'HIDETHEGOLDINTHETREXESTUMP')
 
 """ Test Transposition cipher """
 class TestTranspositionCipher(unittest.TestCase):
@@ -64,5 +72,40 @@ class TestTranspositionCipher(unittest.TestCase):
     def test_decode_unequal_lenght(self):
         self.assertEqual(transposition.decode_transposition('HKSUATSILSEYBE', 'cat'), 'THESKYISBLUESA')
 
+""" Test Vigenere cipher """
+class TestVigenereCipher(unittest.TestCase):
+    @unittest.expectedFailure
+    def test_cli_commands(self):
+        p = subprocess.check_output(['python', 'vigenere.py', '-e', '-d'])
+        self.expectedFailure(p)
+        
+    def test_encryption_basic(self):
+        self.assertEqual(vigenere.encrypt_vigenere('ATTACKATDAWN', 'LEMON'), 'LXFOPVEFRNHR')
+    
+    def test_decryption_basic(self):
+        self.assertEqual(vigenere.decrypt_vigenere('LXFOPVEFRNHR', 'LEMON'), 'ATTACKATDAWN')
+        
+    def test_encryption_basic(self):
+        self.assertEqual(vigenere.encrypt_vigenere('attackatdawn', 'lemon'), 'LXFOPVEFRNHR')
+    
+    def test_decryption_basic(self): 
+        self.assertEqual(vigenere.decrypt_vigenere('lxfopvefrnhr', 'lemon'), 'ATTACKATDAWN')
+        
+    def test_encryption_complex(self):
+        self.assertEqual(vigenere.encrypt_vigenere('TEST{Let\'s_73st_s0meth1ng}', 'Pr0bl3m', \
+        '[a-zA-Z0-9{}]'), 'wVGUjCq8\'J_V4Dk_EDD4usSzVq')
+    
+    def test_decryption_complex(self):
+        self.assertEqual(vigenere.decrypt_vigenere('wVGUjCq8\'J_V4Dk_EDD4usSzVq', 'Pr0bl3m', \
+        '[a-zA-Z0-9{}]'),'TEST{Let\'s_73st_s0meth1ng}')
+        
+    def test_encryption_complex_fail(self):
+        with self.assertRaises(SystemExit):
+            vigenere.encrypt_vigenere('TEST{Let\'s_73st_s0meth1ng}', 'Pr0bl3m', '[a-zA-Z{}]')
+    
+    def test_decryption_complex_fail(self):
+        with self.assertRaises(SystemExit):
+            vigenere.decrypt_vigenere('wVGUjCq8\'J_V4Dk_EDD4usSzVq', 'Pr0bl3m', '[a-zA-Z{}]')
+    
 if __name__ == '__main__':
     unittest.main()
