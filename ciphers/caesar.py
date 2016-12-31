533eb902bebeffc3f4e350ac2d97d41bedef1141
Migdalo <Migdalo@users.noreply.github.com>
2016-12-31 19:06:23 +0200

    Add option to rotate only to chars of same type

--- ciphers/caesar.py
+++ ciphers/caesar.py
@@ -4,7 +4,7 @@ import argparse
 """ Caesar cipher """
 
 """ Get an alphabet range to use. """
-def discover_alphabet(convertable, nonumbers):
+def discover_alphabet(convertable, nonumbers=False, addpunct=False):
     alphabet = []
     has_upper = False
     has_lower = False
@@ -30,36 +30,48 @@ def discover_alphabet(convertable, nonumbers):
         alphabet.append(string.digits)
     return ''.join(alphabet)
 
+""" Rotates a single character. """
+def rotate_char(char, rotation, alphabet):
+    tmp = alphabet.index(char) + rotation
+    while tmp >= len(alphabet):
+        tmp = tmp - len(alphabet)
+    return alphabet[tmp]
+
 """ Rotate characters """
-def rotate(convertable, rot=13, alphabet='', nonumbers=False):
+def rotate(convertable, rot=13, alphabet='', nonumbers=False, sametype=False):
     plain = []
     
-    if not alphabet:
-        alphabet = discover_alphabet(convertable, nonumbers)
-    
-    # If rotation is larger than the alphabet length
-    if rot > len(alphabet):
-        rot = rot - len(alphabet)
+    if not sametype:
+        if not alphabet:
+            alphabet = discover_alphabet(convertable, nonumbers)
+        
+        # If rotation is larger than the alphabet length
+        while rot > len(alphabet):
+            rot = rot - len(alphabet)
     
     # Rotate all characters in the input string
-    try:
-        for c in convertable:
-            if nonumbers and c in string.digits:
-                plain.append(c)
-            elif c not in alphabet:
-                plain.append(c)
+    for c in convertable:
+        if nonumbers and c in string.digits:
+            plain.append(c)
+        elif sametype:
+            if c in string.ascii_uppercase:
+                plain.append(rotate_char(c, rot, string.ascii_uppercase))
+            elif c in string.ascii_lowercase:
+                plain.append(rotate_char(c, rot, string.ascii_lowercase))
+            elif c in string.digits:
+                plain.append(rotate_char(c, rot, string.digits))
             else:
-                tmp = alphabet.index(c) + rot
-                if tmp >= len(alphabet):
-                    tmp = tmp - len(alphabet)
-                plain.append(alphabet[tmp])
-        return ''.join(plain)
-    except:
-        return ''
+                plain.append(c)
+        elif c not in alphabet:
+            plain.append(c)
+        else:
+            plain.append(rotate_char(c, rot, alphabet))
+    return ''.join(plain)
 
 """ Brute-force all possible rotations """
-def solve_rotation(convertable, keyword, nonumbers):
-    alphabet = discover_alphabet(convertable, nonumbers)
+def solve_rotation(convertable, keyword, nonumbers=False, sametype=False):
+    if not sametype:
+        alphabet = discover_alphabet(convertable, nonumbers)
     print('{0: <5}'.format('ROT'), convertable)
     print('-' * 20)
     
@@ -74,22 +86,25 @@ def solve_rotation(convertable, keyword, nonumbers):
                 return
 
 def process_args():
-    parser = argparse.ArgumentParser(description='Encode and decode Caesar cipher.', epilog='Developer: Migdalo')
+    parser = argparse.ArgumentParser(description='Encode and decode Caesar cipher.', epilog='Author: Migdalo')
     parser.add_argument('text', help='The text you want to rotate.')
     group = parser.add_mutually_exclusive_group()
     group.add_argument('-r', '--rotation', type=int, help='Number of rotations you want to do. ' +
             'Use negative numbers to decipher. Default is 13.')
     group.add_argument('-b', '--brute', action='store_true', help='Print all rotation options.')
-    parser.add_argument('-k',  '--keyword', help='Keyword you want to find. Usable with -b.')
+    parser.add_argument('-k', '--keyword', help='Keyword you want to find. Usable with -b.')
     parser.add_argument('-n', '--nonumbers', action='store_true', help='Don\'t rotate numbers.')
+    parser.add_argument('-s', '--sametype', action='store_true', help='Don\'t allow character type to change.' +
+            ' Force uppercase characters to rotate to another uppercase character and so forth.')
     args = parser.parse_args()
     
     if args.brute:
-        solve_rotation(args.text, args.keyword, args.nonumbers)
+        solve_rotation(args.text, args.keyword, args.nonumbers, args.sametype)
     elif args.rotation:
-        print(rotate(args.text, args.rotation, nonumbers=args.nonumbers))
+        print(rotate(args.text, args.rotation, nonumbers=args.nonumbers, sametype=args.sametype))
     else:
-        print(rotate(args.text, nonumbers=args.nonumbers))
+        print(rotate(args.text, nonumbers=args.nonumbers, sametype=args.sametype))
 
 if __name__ == '__main__':
     process_args()
+    
